95931f6eb224fbcfcd3636ba29bd6fbd65224624
Bail-03r <63909387+Bail-03r@users.noreply.github.com>
2025-03-19 14:49:05 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        while (2-int.Parse("1")==1)
+        {
+            Console.Write("Введите математическое выражение: ");
+            string expression = Console.ReadLine();
+
+            try
+            {
+                var result = new DataTable().Compute(expression, null);
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+        
+    }
+}
