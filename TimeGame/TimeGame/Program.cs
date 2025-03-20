088cc9d24f28279f15afc773abd27918ce4877cd
0x8q11 <63909387+Bail-03r@users.noreply.github.com>
2025-03-20 12:05:25 +0300

    Add files via upload

---
+++ TimeGame/TimeGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+
+    static void Main()
+    {
+        string[] actions = { "Прыгать", "обнимать случайного человека", "танцевать в присядку", "петь", "смотреть на случайного человека" };
+
+        Random random = new Random();
+        string randomAction = actions[random.Next(actions.Length)];
+
+        int timeLimitInSeconds = 60;
+
+        Console.WriteLine($"задание: {randomAction}");
+        Console.WriteLine($"у вас есть 1 минута готовьтесь...");
+        Thread.Sleep(3000);
+        for (int i = timeLimitInSeconds; i >= 0; i--)
+        {
+            Console.Clear();
+            Console.WriteLine($"ваше задание: {randomAction} в течении времени");
+            Console.WriteLine($"Осталось времени: {i} секунд");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine("время вышло");
+    }
+}
